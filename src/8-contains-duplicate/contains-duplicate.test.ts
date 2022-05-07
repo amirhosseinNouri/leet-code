a320123957a30b5803cac55641c90c1ba17328e8
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2022-05-07 16:27:43 +0430

    feat: implement contains duplicate

--- src/8-contains-duplicate/contains-duplicate.test.ts
+++ src/8-contains-duplicate/contains-duplicate.test.ts
@@ -4,4 +4,20 @@ describe('containsNearbyDuplicate test', () => {
   test('should return false when array has one element', () => {
     expect(containsNearbyDuplicate([1], 2)).toBe(false);
   });
+
+  test('should return false on empty array', () => {
+    expect(containsNearbyDuplicate([], 2)).toBe(false);
+  });
+
+  test('test case 1', () => {
+    expect(containsNearbyDuplicate([1, 2, 3, 1], 3)).toBe(true);
+  });
+
+  test('test case 2', () => {
+    expect(containsNearbyDuplicate([1, 0, 1, 1], 1)).toBe(true);
+  });
+
+  test('test case 3', () => {
+    expect(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2)).toBe(false);
+  });
 });
--- src/8-contains-duplicate/contains-duplicate.ts
+++ src/8-contains-duplicate/contains-duplicate.ts
@@ -1,4 +1,17 @@
 function containsNearbyDuplicate(nums: number[], k: number): boolean {
+  if (nums.length <= 1) {
+    return false;
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    const currentElement = nums[i];
+    const startIndex = i + 1;
+    const endIndex = i + k > nums.length - 1 ? nums.length - 1 : i + k;
+    if (nums.slice(startIndex, endIndex + 1).indexOf(currentElement) !== -1) {
+      return true;
+    }
+  }
+
   return false;
 }
 
